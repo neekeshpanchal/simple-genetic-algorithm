e192f9e78fc37499a984f207b3473e73ce3146aa
hawk <80868396+acceptablehawk@users.noreply.github.com>
2022-11-14 12:37:14 -0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -1,6 +1,8 @@
-from fuzzywuzzy import fuzz
 import random
 from rosen import rosenbrockvalley
+
+population = 20
+generations = 10000
 
 class Agent:
 
@@ -12,12 +14,8 @@ class Agent:
     def __str__(self):
 
         return 'String: ' + str(self.binary) + ' Fitness: ' + str(self.fitness)
-
-in_str = None
-in_str_len = None
-population = 20
-generations = 10000
-
+    
+    
 def dnageneration():
 
     codes = []
@@ -26,30 +24,26 @@ def dnageneration():
 
     return codes
 
-    
 
 def ga():
 
-    agents = init_agents(population, in_str_len)
+    agents = init_agents(population)
 
     for generation in range(generations):
         print('Generation: ' + str(generation)) 
 
         agents = fitness(agents)
-        agents = selection(agents)
-        agents = crossover(agents)
-        agents = mutation(agents)
 
-        if any(agent.fitness >= 10000000 for agent in agents):
+        if any(agent.fitness >= maximum for agent in agents):
 
-            print('Threshold met!')
+            print('Function has reached its maximum/minimum value.')
             exit(0)
 
 
 def init_agents(population, length):
 
 
-    return [Agent(length) for _ in range(population)]
+    return [Agent(length) for x in range(population)]
 
 
 def fitness(agents):
@@ -57,56 +51,5 @@ def fitness(agents):
     for agent in agents:
 
         agent.fitness = rosenbrockvalley(i,j)
-
-    return agents
-
-
-def selection(agents):
-
-    agents = sorted(agents, key=lambda agent: agent.fitness, reverse=True)
-    print('\n'.join(map(str, agents)))
-    agents = agents[:int(0.2 * len(agents))]
-
-    return agents
-
-
-def crossover(agents):
-
-    offspring = []
-
-    for _ in range(int(population - len(agents) / 2)):
-
-        parent1 = random.choice(agents)
-        parent2 = random.choice(agents)
-        child1 = Agent(in_str_len)
-        child2 = Agent(in_str_len)
-        split = random.randint(0, in_str_len)
-        child1.string = parent1.string[0:split] + parent2.string[split:in_str_len]
-        child2.string = parent2.string[0:split] + parent1.string[split:in_str_len]
-
-        offspring.append(child1)
-        offspring.append(child2)
-
-    agents.extend(offspring)
-
-    return agents
-
-
-def mutation(agents):
-
-    for agent in agents:
-
-        for idx, param in enumerate(agent.string):
-
-            if random.uniform(0.0, 1.0) <= 0.1:
-
-                agent.string = agent.string[0:idx] + random.choice(string.ascii_letters) + agent.string[idx+1:in_str_len]
-
-    return agents
-
-
-if __name__ == '__main__':
-
-    in_str = 'Group 08'
-    in_str_len = len(in_str)
-    ga()
+ 
+ga()
