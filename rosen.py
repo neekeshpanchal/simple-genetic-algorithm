28d894ae5666373e152c9a94c66345a7101a3f3c
hawk <80868396+acceptablehawk@users.noreply.github.com>
2022-11-14 12:38:53 -0800

    Update rosen.py

--- rosen.py
+++ rosen.py
@@ -1,27 +1,8 @@
-import numpy as np
-import pandas as pd 
-import random
+from ga import Agent
 
-
-samples_number = 10000
-
-inputs_number = 2
-
-distribution = random.uniform(-1, 1)
-
-inputs = np.random.uniform(-1.0, 1.0, size = (samples_number, inputs_number))
-
-rosenbrock = []
-
-for j in range (samples_number):
+def rosenbrockvalley(i,j):
     
-    r = 0
-    
-    for i in range(inputs_number-1):
-        
-        r += (1.0 - inputs[j][i])*(1.0 - inputs[j][i])+100.0*((inputs[j][i+1]-inputs[j][i]*inputs[j][i])*(inputs[j][i+1]-inputs[j][i]*inputs[j][i]))
-        
-    rosenbrock.append(r)
+    r += (1.0 - i)*(1.0 - i)+100.0*((j-i*i)*(j-i*i))
 
+    return r
 
-print(rosenbrock)
