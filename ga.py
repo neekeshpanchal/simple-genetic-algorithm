b386f455b58d20ea530477b494ecf39a0492489f
hawk <80868396+acceptablehawk@users.noreply.github.com>
2022-11-15 22:26:47 -0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -1,22 +1,31 @@
 import random
 
-#Global Variables
+#Global Variables and Inputs
 population = 10
 generations = 10000
-num = int(input("How big is your gene pool? \n"))
+num = int(input("Gene Pool Variance (0 - 10 0000):  \n")) #Specifies the domain of values which can be generated. 
+                                                          #     ex.) input = 50 output = (29, 49, 5), (4, 23, 45)..etc, 
 num+=1
 domain = []
 
+indicator = input("""
+Select your Function: \n
+    (r) Rosenbrock's Valley
+    (h) Himmelblau Function
+    (d) De Jong Sphere Function \n
+""")
+
 choice = int(input("""
-Pick your choice of optimization: \n
-(1) Maximize
-(2) Minimize
+Select your choice of optimization: \n
+    (1) Maximize
+    (2) Minimize \n
 """))
 
 for x in range(1, num):
     domain.append(x)
 
-#Organism Class 
+    
+#Organism
 class Organism:
 
     def __init__(self, code):
@@ -35,6 +44,11 @@ def rosenbrockvalley(i,j):
 
     return r
 
+def himmelblau(i,j):
+    h = 0
+    h += ((((i*i) + j - 11)*(i*i) + j - 11) + ((i+(j*j)-7)*(i+(j*j)-7)))
+
+    return h
 
 def dnageneration():
 
@@ -46,18 +60,15 @@ def dnageneration():
         c = random.choice(domain)
         dnalist.append((a,b,c))
 
-
-
     return dnalist
     
 
 def ga():
 
-    indicator = input("Type r for Rosenbrock's Valley:  \n")
-
     organisms = init_organisms(dnageneration())
 
     for generation in range(generations):
+
         print('Generation: ' + str(generation)) 
 
         organisms = fitness(organisms, indicator)
@@ -66,12 +77,12 @@ def ga():
         organisms = mutation(organisms)
 
         if choice == 1:
-            if any(organism.code == (max(domain),max(domain),max(domain)) for organism in organisms):
-                print('Function Maximized on generation ' + str(int(generation+1)))
+            if any(organism.code == (max(domain),max(domain),max(domain)) for organism in organisms): # Determining when the function has reached its maximum value 
+                print('Function Maximized on generation ' + str(int(generation+1)))                   
                 exit(0)
         
         else:
-            if any(organism.code == (min(domain),min(domain),min(domain)) for organism in organisms):
+            if any(organism.code == (min(domain),min(domain),min(domain)) for organism in organisms): # Determining when the function has reached its minimum value 
                 print('Function Minimized on generation ' + str(int(generation+1)))
                 exit(0)
 
@@ -93,6 +104,10 @@ def fitness(organisms, indicator):
         for organism in organisms:
             organism.fitness = rosenbrockvalley((organism.code[0]*organism.code[1]*organism.code[2]), sum(list(organism.code)))
 
+    elif indicator.lower() == 'h':
+        for organism in organisms:
+            organism.fitness = himmelblau((organism.code[0]*organism.code[1]*organism.code[2]), sum(list(organism.code)))
+
     return organisms
 
 
@@ -107,6 +122,7 @@ def selection(organisms):
     else: 
         organisms = organisms[int(0.4 * len(organisms)):]
 
+
     return organisms
 
 
@@ -117,27 +133,49 @@ def crossover(organisms):
 
     for x in range(int((population - len(organisms)) / 2)):
 
-        parent1 = organisms[0]
-        parent2 = organisms[1]
-        child1 = Organism(None)
-        child2 = Organism(None)
-        split = random.randint(0, 3)
-        child1.code = parent1.code[0:split] + parent2.code[split:3]
-        child2.code = parent2.code[0:split] + parent1.code[split:3]
+        if choice == 1:
+            parent1 = organisms[0]
+            parent2 = organisms[1]
+            child1 = Organism(None)
+            child2 = Organism(None)
+            split = random.randint(0, 3)
+            child1.code = parent1.code[0:split] + parent2.code[split:3]
+            child2.code = parent2.code[0:split] + parent1.code[split:3]
 
-        parent3 = organisms[2]
-        parent4 = organisms[3]
-        child3 = Organism(None)
-        child4 = Organism(None)
-        split = random.randint(0, 3)
-        child3.code = parent3.code[0:split] + parent4.code[split:3]
-        child4.code = parent4.code[0:split] + parent3.code[split:3]
+            parent3 = organisms[2]
+            parent4 = organisms[3]
+            child3 = Organism(None)
+            child4 = Organism(None)
+            split = random.randint(0, 3)
+            child3.code = parent3.code[0:split] + parent4.code[split:3]
+            child4.code = parent4.code[0:split] + parent3.code[split:3]
 
-        offspring.append(child1)
-        offspring.append(child2)
-        offspring.append(child3)
-        offspring.append(child4)
+            offspring.append(child1)
+            offspring.append(child2)
+            offspring.append(child3)
+            offspring.append(child4)
 
+        else:
+            parent1 = organisms[-1]
+            parent2 = organisms[-2]
+            child1 = Organism(None)
+            child2 = Organism(None)
+            split = random.randint(0, 3)
+            child1.code = parent1.code[0:split] + parent2.code[split:3]
+            child2.code = parent2.code[0:split] + parent1.code[split:3]
+
+            parent3 = organisms[-3]
+            parent4 = organisms[-4]
+            child3 = Organism(None)
+            child4 = Organism(None)
+            split = random.randint(0, 3)
+            child3.code = parent3.code[0:split] + parent4.code[split:3]
+            child4.code = parent4.code[0:split] + parent3.code[split:3]
+
+            offspring.append(child1)
+            offspring.append(child2)
+            offspring.append(child3)
+            offspring.append(child4)
     
     organisms.extend(offspring)
 
